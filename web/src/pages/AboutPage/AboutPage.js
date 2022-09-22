4edd188d6ab08541931e72e62f5467a478340546
Eric Lake <ericallenlake@gmail.com>
2022-09-21 23:41:09 -0600

    Finishes ch 1 and starts 2 in Redwood tutorial

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,16 @@
+
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <p>
+        Playing around with Redwood. So far it's really helping with config which is my least favorite part of programming.
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
