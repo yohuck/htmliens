d5a07fd0b514532abaf14551875d6c321cfa7de0
Eric Lake <ericallenlake@gmail.com>
2022-09-27 21:40:58 -0600

    Preps for deploy

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -15,7 +15,7 @@ const BlogLayout = ({ children }) => {
           </h1>
           {isAuthenticated ? (
             <div>
-              <span>Logged in as {currentUser.email}</span>{' '}
+              <span className="login">Logged in as {currentUser.email}</span>{' '}
               <button type="button" onClick={logOut}>
                 Logout
               </button>
